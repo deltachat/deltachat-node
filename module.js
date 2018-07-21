b069281004ff557a7d8d2acf5121a593d42df382
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-07-21 23:45:13 +0200

    Load bindings using node-gyp-build

--- module.js
+++ module.js
@@ -1,10 +1,9 @@
-const addon = require('./build/Release/module');
+const binding = require('node-gyp-build')(__dirname)
+const context = binding.dc_context_new()
 
-let dc_context = addon.dc_context_new();
+console.log('context', context)
+console.log(binding.dc_set_event_handler_cb(context, event => {
+  console.log("this is really js event int", event)
+}))
 
-console.log(dc_context);
-console.log(addon.dc_set_event_handler_cb(dc_context, (eventInteger) => {
-  console.log("this is really js event int", eventInteger); 
-}));
-
-console.log(addon.dc_perform_jobs_start(dc_context));
+console.log(binding.dc_perform_jobs_start(context))
