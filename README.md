3b1882ad82077ff2398fda119704b31102e977ce
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-07-21 23:51:04 +0200

    Add standard

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 > node.js bindings for deltachat-core based on napi
 
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
 ## License
+
 GPLv3
 
--- module.js
+++ module.js
@@ -3,7 +3,7 @@ const context = binding.dc_context_new()
 
 console.log('context', context)
 console.log(binding.dc_set_event_handler_cb(context, event => {
-  console.log("this is really js event int", event)
+  console.log('this is really js event int', event)
 }))
 
 console.log(binding.dc_perform_jobs_start(context))
