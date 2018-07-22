dd0d82b58533c45edb09b977b85aea9389b81400
jikstra <jikstra@disroot.org>
2018-07-22 19:03:00 +0200

    Started working on getting back the context into the javascript context

--- example.js
+++ example.js
@@ -1,9 +1,14 @@
-const dc = require('.')
+const binding = require('.')
 
-console.log('result of dc.create', dc.create())
 
-console.log('result from set_event_handler:', dc.set_event_handler(event => {
+let dc_context = binding.dcn_context_new()
+console.log('result of dcn_context_new', dc_context)
+
+let result
+result = binding.dcn_set_event_handler(event => {
   console.log('this is really js event int', event)
-}))
+})
+console.log('result from set_event_handler:', result)
 
-console.log('result from start_threads:', dc.start_threads())
+result = binding.dcn_start_threads()
+console.log('result from start_threads:', result)
--- src/module.c
+++ src/module.c
@@ -5,27 +5,26 @@
 #include <deltachat.h>
 #include <pthread.h>
 
-// Context from deltachat-core library.
-static dc_context_t* dc_context_g = NULL;
+// Context struct we need for some binding specific things. dcn_context_t will
+// be applied to the dc_context created in dcn_context_new().
+typedef struct dcn_context_t {
+  napi_value napi_threadsafe_function_event_handler;
+  pthread_mutex_t mutex_g;
+  int current_event_g;
+} dcn_context_t;
 
-static napi_value napi_threadsafe_function_event_handler = NULL;
-
-static pthread_mutex_t mutex_g = PTHREAD_MUTEX_INITIALIZER;
-
-// Current event in dc_event_handler()
-static int current_event_g;
 
 uintptr_t dc_event_handler(dc_context_t* context, int event, uintptr_t data1, uintptr_t data2)
 {
   printf("dc_event_handler, event: %d\n", event);
 
-  if (napi_threadsafe_function_event_handler != NULL) {
+  /*if (napi_threadsafe_function_event_handler != NULL) {
     pthread_mutex_lock(&mutex_g);
     printf("a %d\n", event);
     current_event_g = event;
     // TODO investigate what the data parameter below does (NULL)
     napi_call_threadsafe_function(napi_threadsafe_function_event_handler, NULL, napi_tsfn_blocking);
-  }
+  }*/
 
   return 0;
 }
@@ -37,7 +36,7 @@ void my_finalize(napi_env env, void* finalize_data, void* finalize_hint)
 
 void my_callback(napi_env env, napi_value js_callback, void* context, void* data)
 {
-  napi_value global;
+  /*napi_value global;
   napi_status status = napi_get_global(env, &global);
 
   if (status != napi_ok) {
@@ -64,16 +63,27 @@ void my_callback(napi_env env, napi_value js_callback, void* context, void* data
     napi_throw_error(env, NULL, "Unable to call event_handler callback");
   }
 
+
   printf("b %d\n", current_event_g);
-  pthread_mutex_unlock(&mutex_g);
+  //pthread_mutex_unlock(&mutex_g);*/
+
 }
 
-NAPI_METHOD(create) {
-  dc_context_g = dc_context_new(dc_event_handler, NULL, NULL);
-  NAPI_RETURN_INT32(1);
+NAPI_METHOD(dcn_context_new) {
+  dc_context_t* dc_context = dc_context_new(dc_event_handler, NULL, NULL);
+  
+  napi_value dc_context_napi;
+  napi_status status = napi_create_external(env, dc_context, NULL, NULL, &dc_context_napi);
+
+  if (status != napi_ok) {
+    napi_throw_error(env, NULL, "Unable to create external dc_context object");
+  }
+
+
+  return dc_context_napi;
 }
 
-NAPI_METHOD(set_event_handler) {
+NAPI_METHOD(dcn_set_event_handler) {
   NAPI_ARGV(1);
 
   napi_value callback = argv[0];
@@ -81,7 +91,7 @@ NAPI_METHOD(set_event_handler) {
 
   NAPI_STATUS_THROWS(napi_create_string_utf8(env, "dc_event_callback", NAPI_AUTO_LENGTH, &async_resource_name));
 
-  NAPI_STATUS_THROWS(napi_create_threadsafe_function(
+  /*NAPI_STATUS_THROWS(napi_create_threadsafe_function(
     env,
     callback,
     0,
@@ -93,26 +103,27 @@ NAPI_METHOD(set_event_handler) {
     NULL,
     my_callback,
     &napi_threadsafe_function_event_handler));
+  */
 }
 
 void* imap_thread_func(void* arg)
 {
-  while (true) {
+  /*while (true) {
     dc_perform_imap_jobs(dc_context_g);
     dc_perform_imap_fetch(dc_context_g);
     dc_perform_imap_idle(dc_context_g);
-  }
+  }*/
 }
 
 void* smtp_thread_func(void* arg)
 {
-  while (true) {
+  /*while (true) {
     dc_perform_smtp_jobs(dc_context_g);
     dc_perform_smtp_idle(dc_context_g);
-  }
+  }*/
 }
 
-NAPI_METHOD(start_threads) {
+NAPI_METHOD(dcn_start_threads) {
   pthread_t imap_thread;
   pthread_create(&imap_thread, NULL, imap_thread_func, NULL);
 
@@ -121,7 +132,7 @@ NAPI_METHOD(start_threads) {
 }
 
 NAPI_INIT() {
-  NAPI_EXPORT_FUNCTION(create);
-  NAPI_EXPORT_FUNCTION(set_event_handler);
-  NAPI_EXPORT_FUNCTION(start_threads);
+  NAPI_EXPORT_FUNCTION(dcn_context_new);
+  NAPI_EXPORT_FUNCTION(dcn_set_event_handler);
+  NAPI_EXPORT_FUNCTION(dcn_start_threads);
 }
