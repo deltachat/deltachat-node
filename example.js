bb5991f099b5e607ed077da03be54010c8de5f05
jikstra <jikstra@disroot.org>
2018-07-22 21:40:34 +0200

    Get rid of current_event variables in dcn_context_t and pass all event handler arguments to javascript callback

--- example.js
+++ example.js
@@ -5,8 +5,8 @@ let dc_context = binding.dcn_context_new()
 console.log('result of dcn_context_new', dc_context)
 
 let result
-result = binding.dcn_set_event_handler(dc_context, event => {
-  console.log('this is really js event int', event)
+result = binding.dcn_set_event_handler(dc_context, (event, data1, data2) => {
+  console.log('this is really js event int', event, data1, data2)
 })
 console.log('result from set_event_handler:', result)
 
--- src/module.c
+++ src/module.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <node_api.h>
 #include <napi-macros.h>
 #include <pthread.h>
@@ -12,14 +13,21 @@
 typedef struct dcn_context_t {
   dc_context_t* dc_context;
   napi_threadsafe_function napi_event_handler;
-  int current_event;
   pthread_mutex_t mutex;
 } dcn_context_t;
 
-static void my_callback(napi_env env, napi_value js_callback, void* context, void* data)
+typedef struct dcn_event_t {
+  int event;
+  uintptr_t data1_int;
+  uintptr_t data2_int;
+  char* data2_str;
+} dcn_event_t;
+
+static void call_js_event_handler(napi_env env, napi_value js_callback, void* context, void* data)
 {
   dcn_context_t* dcn_context = (dcn_context_t*)context;
-  
+  dcn_event_t* dcn_event = (dcn_event_t*)data;
+
   napi_value global;
   napi_status status = napi_get_global(env, &global);
 
@@ -27,11 +35,34 @@ static void my_callback(napi_env env, napi_value js_callback, void* context, voi
     napi_throw_error(env, NULL, "Unable to get global");
   }
 
-  napi_value argv[1];
-  status = napi_create_int32(env, dcn_context->current_event, &argv);
+  const int argc = 3;
+  napi_value argv[argc];
+  
+  status = napi_create_int32(env, dcn_event->event, &argv[0]);
   if (status != napi_ok) {
     napi_throw_error(env, NULL, "Unable to create argv[0] for event_handler arguments");
   }
+
+  status = napi_create_int32(env, dcn_event->data1_int, &argv[1]);
+  if (status != napi_ok) {
+    napi_throw_error(env, NULL, "Unable to create argv[1] for event_handler arguments");
+  }
+
+  if (dcn_event->data2_str) {
+    status = napi_create_string_utf8(env, dcn_event->data2_str, NAPI_AUTO_LENGTH, &argv[2]);
+    if (status != napi_ok) {
+      napi_throw_error(env, NULL, "Unable to create argv[2] for event_handler arguments");
+    }
+    free(dcn_event->data2_str);
+  } else {
+    status = napi_create_int32(env, dcn_event->data2_int, &argv[2]);
+    if (status != napi_ok) {
+      napi_throw_error(env, NULL, "Unable to create argv[2] for event_handler arguments");
+    }
+  }
+
+  free(dcn_event);
+  dcn_event = NULL;
 
   napi_value result;
 
@@ -39,7 +70,7 @@ static void my_callback(napi_env env, napi_value js_callback, void* context, voi
     env,
     global,
     js_callback,
-    1,
+    argc,
     argv,
     &result);
 
@@ -47,10 +78,7 @@ static void my_callback(napi_env env, napi_value js_callback, void* context, voi
     napi_throw_error(env, NULL, "Unable to call event_handler callback");
   }
 
-
-  printf("b %d\n", dcn_context->current_event);
   pthread_mutex_unlock(&dcn_context->mutex);
-
 }
 
 uintptr_t dc_event_handler(dc_context_t* dc_context, int event, uintptr_t data1, uintptr_t data2)
@@ -61,10 +89,14 @@ uintptr_t dc_event_handler(dc_context_t* dc_context, int event, uintptr_t data1,
   if (dcn_context->napi_event_handler != NULL) {
     pthread_mutex_lock(&dcn_context->mutex);
     printf("a %d\n", event);
-    // TODO Take care of strings in data1/2 when putting into dcn_context
-    dcn_context->current_event = event;
-    // TODO investigate what the data parameter below does (NULL)
-    napi_call_threadsafe_function(dcn_context->napi_event_handler, NULL, napi_tsfn_blocking);
+    
+    dcn_event_t* dcn_event = calloc(1, sizeof(dcn_event_t));
+    dcn_event->event = event;
+    dcn_event->data1_int = data1;
+    dcn_event->data2_int = data2;
+    dcn_event->data2_str = (DC_EVENT_DATA2_IS_STRING(event) && data2) ? strdup((char*)data2) : NULL;
+        
+    napi_call_threadsafe_function(dcn_context->napi_event_handler, dcn_event, napi_tsfn_blocking);
   }
 
   return 0;
@@ -81,7 +113,6 @@ NAPI_METHOD(dcn_context_new) {
   dcn_context_t* dcn_context = calloc(1, sizeof(dcn_context_t));
   dcn_context->dc_context = NULL; 
   dcn_context->napi_event_handler = NULL;
-  dcn_context->current_event = 0;
   pthread_mutex_init(&dcn_context->mutex, NULL);
   
   dcn_context->dc_context = dc_context_new(dc_event_handler, dcn_context, NULL);
@@ -120,9 +151,9 @@ NAPI_METHOD(dcn_set_event_handler) {
     0,
     3,
     0,
-    my_finalize,
+    NULL, // TODO: file an issue that the finalize parameter should be optional
     dcn_context,
-    my_callback,
+    call_js_event_handler,
     &dcn_context->napi_event_handler));
 
   return 0;
