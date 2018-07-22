6868411fd7e9d4f427504bdedee7201d92435e52
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-07-23 01:22:08 +0200

    Use dcn_context in example

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
 const binding = require('.')
 
 
-let dc_context = binding.dcn_context_new()
-console.log('result of dcn_context_new', dc_context)
+let dcn_context = binding.dcn_context_new()
+console.log('result of dcn_context_new', dcn_context)
 
 let result
-result = binding.dcn_set_event_handler(dc_context, (event, data1, data2) => {
+result = binding.dcn_set_event_handler(dcn_context, (event, data1, data2) => {
   console.log('this is really js event int', event, data1, data2)
 })
 console.log('result from set_event_handler:', result)
 
-result = binding.dcn_start_threads(dc_context)
+result = binding.dcn_start_threads(dcn_context)
 console.log('result from start_threads:', result)
