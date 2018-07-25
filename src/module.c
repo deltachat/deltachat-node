84bc6b80db03600f6da3201f9a7c35a1553904f5
Jikstra <34889164+Jikstra@users.noreply.github.com>
2018-07-25 12:54:46 +0200

    Put napi macros in own header file (#17)

--- src/module.c
+++ src/module.c
@@ -4,31 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <node_api.h>
-#include <napi-macros.h>
 #include <pthread.h>
 #include <deltachat.h>
-
-#undef NAPI_UTF8
-#define NAPI_UTF8(name, val) \
-  size_t name##_size = 0; \
-  NAPI_STATUS_THROWS(napi_get_value_string_utf8(env, val, NULL, 0, &name##_size)); \
-  name##_size++; \
-  char name[name##_size]; \
-  size_t name##_len; \
-  NAPI_STATUS_THROWS(napi_get_value_string_utf8(env, val, (char *) &name, name##_size, &name##_len));
-
-#define NAPI_DCN_CONTEXT() \
-  dcn_context_t* dcn_context; \
-  NAPI_STATUS_THROWS(napi_get_value_external(env, argv[0], (void**)&dcn_context));
-
-#define NAPI_RETURN_UNDEFINED() \
-  return 0;
-
-#define NAPI_RETURN_AND_FREE_STRING(name) \
-  napi_value return_utf8; \
-  napi_create_string_utf8(env, name, NAPI_AUTO_LENGTH, &return_utf8); \
-  free(name); \
-  return return_utf8;
+#include "napi-macros.h"
 
 // Context struct we need for some binding specific things. dcn_context_t will
 // be applied to the dc_context created in dcn_context_new().
