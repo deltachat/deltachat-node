da604f8ff26f2ed5af586e2a9948a6a34057e862
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-07-30 14:35:04 +0200

    Remove example.js in favor of test/index.js

--- example.js
+++
@@ -1,9 +0,0 @@
-const DeltaChat = require('.')
-const minimist = require('minimist')
-
-let dc = new DeltaChat(minimist(process.argv.slice(2)))
-
-dc.on('open', () => {
-  console.log('open done')
-  dc.close()
-})
