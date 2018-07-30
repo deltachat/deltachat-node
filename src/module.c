503f45e0b3dfcae55cd74007a8c3d8dbf7bb0292
Jikstra <34889164+Jikstra@users.noreply.github.com>
2018-07-30 02:32:38 +0200

    Implement async callback for dcn_open method (#50)

--- src/module.c
+++ src/module.c
@@ -131,7 +131,7 @@ static void smtp_thread_func(void* arg)
   dc_context_t* dc_context = dcn_context->dc_context;
 
   napi_acquire_threadsafe_function(dcn_context->threadsafe_event_handler);
-
+  
   while (dcn_context->loop_thread) {
     dc_perform_smtp_jobs(dc_context);
     dc_perform_smtp_idle(dc_context);
@@ -344,7 +344,7 @@ NAPI_METHOD(dcn_get_config_int) {
   NAPI_INT32(def, argv[2]);
 
   int value = dc_get_config_int(dcn_context->dc_context, key, def);
-
+  
   free(key);
 
   NAPI_RETURN_INT32(value);
@@ -487,19 +487,68 @@ NAPI_METHOD(dcn_msg_new) {
   return result;
 }
 
+typedef struct dcn_open_carrier_t {
+  dcn_context_t* dcn_context;
+  char* dbfile;
+  char* blobdir;
+  napi_ref callback_ref;
+  napi_async_work async_work;
+  int dc_open_status;
+} dcn_open_carrier_t;
+
+void dcn_open_execute(napi_env env, void* data) {
+  dcn_open_carrier_t* dcn_open_carrier = (dcn_open_carrier_t*)data;
+  
+  // blobdir may be the empty string or NULL for default blobdir
+  dcn_open_carrier->dc_open_status = dc_open(
+      dcn_open_carrier->dcn_context->dc_context,
+      dcn_open_carrier->dbfile,
+      dcn_open_carrier->blobdir);
+}
+
+void dcn_open_complete(napi_env env, napi_status status, void* data) {
+  dcn_open_carrier_t* dcn_open_carrier = (dcn_open_carrier_t*)data;
+
+  if (status != napi_ok) {
+    napi_throw_type_error(env, NULL, "Execute callback failed.");
+    return;
+  }
+
+  const int argc = 1;
+  napi_value argv[argc];
+  NAPI_STATUS_THROWS(napi_create_int32(env, dcn_open_carrier->dc_open_status, &argv[0]));
+
+  napi_value global;
+  NAPI_STATUS_THROWS(napi_get_global(env, &global));
+  napi_value callback;
+  NAPI_STATUS_THROWS(napi_get_reference_value(env, dcn_open_carrier->callback_ref, &callback));
+  napi_value result;
+  NAPI_STATUS_THROWS(napi_call_function(env, global, callback, argc, argv, &result));
+
+  //NAPI_STATUS_THROWS(napi_delete_reference(env, &dcn_open_carrier->callback_ref));
+  //NAPI_STATUS_THROWS(napi_delete_async_work(env, dcn_open_carrier->async_work));
+}
+
 NAPI_METHOD(dcn_open) {
-  NAPI_ARGV(3);
+  NAPI_ARGV(4);
   NAPI_DCN_CONTEXT();
   NAPI_UTF8(dbfile, argv[1]);
   NAPI_UTF8(blobdir, argv[2]);
+  napi_value callback = argv[3];
 
-  // blobdir may be the empty string or NULL for default blobdir
-  int status = dc_open(dcn_context->dc_context, dbfile, blobdir);
 
-  free(dbfile);
-  free(blobdir);
+  dcn_open_carrier_t* dcn_open_carrier = calloc(1, sizeof(dcn_open_carrier_t));
+  dcn_open_carrier->dcn_context = dcn_context;
+  dcn_open_carrier->dbfile = dbfile;
+  dcn_open_carrier->blobdir = blobdir;
 
-  NAPI_RETURN_INT32(status);
+  napi_value async_resource_name;
+  NAPI_STATUS_THROWS(napi_create_reference(env, argv[3], 1, &dcn_open_carrier->callback_ref));
+  NAPI_STATUS_THROWS(napi_create_string_utf8(env, "dcn_open_callback", NAPI_AUTO_LENGTH, &async_resource_name));
+  NAPI_STATUS_THROWS(napi_create_async_work(env, callback, async_resource_name, dcn_open_execute, dcn_open_complete, dcn_open_carrier, &dcn_open_carrier->async_work));
+  NAPI_STATUS_THROWS(napi_queue_async_work(env, dcn_open_carrier->async_work));
+
+  return NULL;
 }
 
 //NAPI_METHOD(dcn_remove_contact_from_chat) {}
