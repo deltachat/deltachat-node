2309ee3b68849736add47ec68671131acdf72fce
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-08-03 13:30:38 +0200

    Parse deltachat-core/src/deltachat.h and generate constants.js

---
+++ scripts/generate-constants.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const split = require('split2')
+
+const rows = []
+const regex = /^#define\s+(\w+)\s+(\w+)/i
+const header = path.resolve(__dirname, '../deltachat-core/src/deltachat.h')
+
+fs.createReadStream(header)
+  .pipe(split())
+  .on('data', line => {
+    const match = regex.exec(line)
+    if (match) {
+      rows.push({ key: match[1], value: parseInt(match[2]) })
+    }
+  })
+  .on('end', () => {
+    const file = path.resolve(__dirname, '../constants.js')
+    const data = rows.map(row => {
+      return `  ${row.key}: ${row.value}`
+    }).join(',\n')
+    fs.writeFileSync(file, `// Generated!\n\nmodule.exports = {\n${data}\n}\n`)
+  })
