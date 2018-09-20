ab4ba6606e1ffc512c219048ef2134ee7adb8460
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-09-20 02:39:59 +0200

    Move out code to constants.js

--- test/index.js
+++ test/index.js
@@ -6,9 +6,9 @@ const tempy = require('tempy')
 const path = require('path')
 const fs = require('fs')
 const c = require('../constants')
-const events = require('../events')
 
 require('./open')
+require('./constants')
 
 let dc = null
 
@@ -367,21 +367,6 @@ test('ChatList methods', t => {
   chatList = dc.getChatList(c.DC_GCL_ARCHIVED_ONLY, 'groupchat1')
   t.is(chatList.getCount(), 1, 'only one archived')
 
-  t.end()
-})
-
-test('reverse lookup of events', t => {
-  const eventKeys = Object.keys(events).map(k => Number(k))
-  const eventValues = Object.values(events)
-  const reverse = eventValues.map(v => c[v])
-  t.same(reverse, eventKeys, 'reverse lookup ok')
-  t.end()
-})
-
-test('event constants are consistent', t => {
-  const eventKeys = Object.keys(c).filter(k => k.startsWith('DC_EVENT_')).sort()
-  const eventValues = Object.values(events).sort()
-  t.same(eventKeys, eventValues, 'identical')
   t.end()
 })
 
