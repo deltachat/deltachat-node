83691dd00fc6c37815f6fa5fc1db6506af50f03c
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-11-25 17:11:32 +0100

    Add Prebuildify (#191)
    * Add prebuildify + tweak .npmignore
    
    * dependency-check should ignore prebuildify
    
    * remove printenv
    
    * Add prebuildify script that builds, creates release and uploads
    
    * Check if changed
    
    * Add GH_TOKEN credentials
    
    * Gzip prebuilds
    
    * No longer need to ignore prebuildify in dependency-check
    
    * change install script to use node-gyp-build + preinstall script for only core
    
    * Clean up + re-order scripts alphabetically + docs
    
    * update ignore files
    
    * Make sure to delete build/ after we are done
    
    * And remove prebuilds/ after done

--- scripts/rebuild-core.js
+++ scripts/rebuild-core.js
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
+
 const path = require('path')
 const rimraf = require('rimraf')
 const mkdirp = require('mkdirp')
 const spawnSync = require('child_process').spawnSync
 
 const verbose = process.env.npm_config_loglevel === 'verbose'
-const debug = process.env.npm_config_debug === 'true'
-
 const coreBuildDir = path.resolve(__dirname, '../deltachat-core/builddir')
 log(`>> Removing ${coreBuildDir}`)
 rimraf.sync(coreBuildDir)
@@ -23,18 +23,6 @@ spawn('meson', mesonArgs, mesonOpts)
 
 spawn('ninja', verbose ? [ '-v' ] : [], {
   cwd: coreBuildDir,
-  stdio: 'inherit'
-})
-
-const buildDir = path.resolve(__dirname, '../build')
-log(`>> Removing ${buildDir}`)
-rimraf.sync(buildDir)
-
-const gypArgs = [ 'rebuild' ]
-if (debug) gypArgs.push('--debug')
-if (verbose) gypArgs.push('--verbose')
-spawn('node-gyp', gypArgs, {
-  cwd: path.resolve(__dirname, '..'),
   stdio: 'inherit'
 })
 
