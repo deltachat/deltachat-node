7d2884eb7323276b8c914f4611453e7ba6d86bd4
jikstra <jikstra@disroot.org>
2019-03-22 20:20:33 +0100

    add test script

---
+++ test.js
@@ -0,0 +1,30 @@
+const deltachat = require('./index')
+const binding = require('./binding')
+const C = require('./constants')
+
+function setup_dc(cb) {
+  let dc = new deltachat()
+  
+  dc.on('ALL', (event, data1, data2) => {
+    //console.log(event, data1, data2)
+  })
+
+  dc.open('/home/kerle/.config/DeltaChat/6a696b7374726140646973726f6f742e6f7267', (err) => {
+    if(err) throw err
+    console.log('Opened!')
+    cb(dc)
+  })
+}
+
+function main(dc) {
+  let chats = dc.getChats()
+  //let msg = binding.dcn_msg_new(dc.dcn_context, C.DC_MSG_TEXT)
+  //binding.dcn_msg_set_text(msg, 'test237')
+  //binding.dcn_set_draft(dc.dcn_context, 13, msg)
+
+  const a = binding.dcn_get_locations(dc.dcn_context, 255, 10)
+  console.log(a)
+}
+
+setup_dc(main)
+
