2b78e0edbb20d4ee2c1a120ee96da516b623a693
Jikstra <34889164+Jikstra@users.noreply.github.com>
2019-04-27 01:46:32 +0300

    Fix scripts, refactor and split out duplicated helper methods (#284)
    * Remove node-gyp rebuild from rebuild-core.js script
    
    * Refactor scripts
    
    - add _commons.js which holds shared helper methods for scripts
    - fix standard
    
    * Don't run npm install with verbose

--- scripts/npm-install.js
+++ scripts/npm-install.js
@@ -1,7 +1,5 @@
 const path = require('path')
-const spawnSync = require('child_process').spawnSync
-
-const verbose = isVerbose()
+const { spawn } = require('./_commons.js')
 
 if (process.env.npm_config_dc_system_lib === 'true') {
   spawn('npx', [ 'node-gyp', 'rebuild', '--', '-Dsystem_dc_core=true' ], {
@@ -13,25 +11,4 @@ if (process.env.npm_config_dc_system_lib === 'true') {
     cwd: path.resolve(__dirname, '../'),
     stdio: 'inherit'
   })
-}
-
-function spawn (cmd, args, opts) {
-  log(`>> ${cmd}`)
-  const result = spawnSync(cmd, args, opts)
-  if (result.status === null) {
-    console.error(`Could not find ${cmd}`)
-    process.exit(1)
-  } else if (result.status !== 0) {
-    console.error(`${cmd} failed with code ${result.status}`)
-    process.exit(1)
-  }
-}
-
-function log (...args) {
-  if (verbose) console.log(...args)
-}
-
-function isVerbose () {
-  const loglevel = process.env.npm_config_loglevel
-  return loglevel === 'verbose' || process.env.CI === 'true'
 }
--- scripts/rebuild-core.js
+++ scripts/rebuild-core.js
@@ -1,9 +1,8 @@
 const path = require('path')
 const rimraf = require('rimraf')
 const mkdirp = require('mkdirp')
-const spawnSync = require('child_process').spawnSync
+const { verbose, log, spawn } = require('./_commons.js')
 
-const verbose = isVerbose()
 const coreBuildDir = path.resolve(__dirname, '../deltachat-core/builddir')
 log(`>> Removing ${coreBuildDir}`)
 rimraf.sync(coreBuildDir)
@@ -24,29 +23,3 @@ spawn('ninja', verbose ? [ '-v' ] : [], {
   cwd: coreBuildDir,
   stdio: 'inherit'
 })
-
-spawn('npx', [ 'node-gyp', 'rebuild' ], {
-  cwd: path.resolve(__dirname, '../'),
-  stdio: 'inherit'
-})
-
-function spawn (cmd, args, opts) {
-  log(`>> ${cmd}`)
-  const result = spawnSync(cmd, args, opts)
-  if (result.status === null) {
-    console.error(`Could not find ${cmd}`)
-    process.exit(1)
-  } else if (result.status !== 0) {
-    console.error(`${cmd} failed with code ${result.status}`)
-    process.exit(1)
-  }
-}
-
-function log (...args) {
-  if (verbose) console.log(...args)
-}
-
-function isVerbose () {
-  const loglevel = process.env.npm_config_loglevel
-  return loglevel === 'verbose' || process.env.CI === 'true'
-}
