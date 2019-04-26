6d4d04225aa327290fc2208fb8c342c4db09d89f
jikstra <jikstra@disroot.org>
2019-04-26 18:03:28 +0300

    refactor code

--- scripts/_otools/parse-otool.js
+++ scripts/_otools/parse-otool.js
@@ -4,55 +4,26 @@ const fs = require('fs-extra')
 
 const librariesToBundle = ['libsasl', 'libssl', 'libcrypto', 'libsqlite', 'libz']
 
-var str = `deltachat.node:
-	/System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation (compatibility version 150.0.0, current version 1349.93.0)
-	/System/Library/Frameworks/CoreServices.framework/Versions/A/CoreServices (compatibility version 1.0.0, current version 775.20.0)
-	/System/Library/Frameworks/Security.framework/Versions/A/Security (compatibility version 1.0.0, current version 57740.60.29)
-	/usr/lib/libsasl2.2.dylib (compatibility version 3.0.0, current version 3.15.0)
-	/usr/lib/libssl.0.9.8.dylib (compatibility version 0.9.8, current version 0.9.8)
-	/usr/lib/libsqlite3.dylib (compatibility version 9.0.0, current version 254.8.0)
-	/usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1238.60.2)
-	/usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 307.5.0)`
-
 function parseOtool(output) {
   outputSplitted = output.split('\n')
   parsed = {}
   parsed.libraryName = outputSplitted[0].substring(0, outputSplitted[0].length -1)
   parsed.libs = []
   for(let i = 1; i < outputSplitted.length; i++) {
-    let libPath = parseOtoolLibLine(outputSplitted[i])
-    parsed.libs.push({libPath: libBasename: path.basename(libpath)})
+    let path = outputSplitted[i].match(/\t(.*)\s\((.*) (.*)\)/)[1]
+    parsed.libs.push({path, basename: path.basename(path)})
   }
   return parsed
 }
 
-function parseOtoolLibLine(libLine) {
-  return libLine.match(/\t(.*)\s\((.*) (.*)\)/)[1]
-}
-
 function otool(file) {
   let cmd = execSync(`otool -L "${file}"`)
-  console.log(cmd)
-}
-
-function installNameToolChange(file, oldLib, newLib) {
-  let cmd = execSync(`install_name_tool -change "${oldLib}" "${newLib}" "${file}"`)
-}
-
-function patchBundleLibraries(libFile, librariesToPatch) {
-  library = otool(libFile)
-  for(let lib of library.libs) {
-    if(!strContainsAny(lib.libBasename, librariesToBundle)) continue
-    
-    copyLibIfNotExists(lib)
-    installNameToolChange(`./${lib.libBasename}`, lib.libPath, `@loader_path/${lib.libBasename}`)
-    patchBundleLibraries(lib.libBasename, librariesToPatch) 
-  }
+  return parseOtool(cmd.stdout)
 }
 
 function copyLibIfNotExists(lib) {
-  if(fs.existsSync(`./${lib.libBasename}`)) return
-  fs.copy(lib.libPath, '.')
+  if(fs.existsSync(`./${lib.basename}`)) return
+  fs.copy(lib.path, '.')
 }
 
 function strContainsAny(str, any) {
@@ -61,6 +32,21 @@ function strContainsAny(str, any) {
   }
   return false
 }
+function installNameToolChange(file, oldLib, newLib) {
+  let cmd = execSync(`install_name_tool -change "${oldLib}" "${newLib}" "${file}"`)
+}
+
+function patchBundleLibraries(libFile, librariesToPatch) {
+  library = otool(libFile)
+  for(let lib of library.libs) {
+    if(!strContainsAny(lib.basename, librariesToBundle)) continue
+    
+    copyLibIfNotExists(lib)
+    installNameToolChange(`./${libFile}`, lib.path, `@loader_path/${lib.basename}`)
+    patchBundleLibraries(lib.basename, librariesToPatch) 
+  }
+}
+
 
 //console.log(parseOtool(str))
 
