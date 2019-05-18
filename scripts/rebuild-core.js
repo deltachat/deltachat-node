feb95308bc00ca419675e3201d69e3414c67d25a
Lars-Magnus Skog <ralphtheninja@riseup.net>
2019-05-18 23:27:18 +0200

    chore: call cargo update before build (#324)
    * chore: call cargo update before build
    
    * refactor opts

--- scripts/rebuild-core.js
+++ scripts/rebuild-core.js
@@ -1,17 +1,16 @@
 const path = require('path')
 const { spawn } = require('./_commons.js')
-const coreDir = path.resolve(__dirname, '../deltachat-core-rust')
+const opts = {
+  cwd: path.resolve(__dirname, '../deltachat-core-rust'),
+  stdio: 'inherit'
+}
 
-spawn(
-  'cargo', [
-    'build',
-    '--release',
-    '--features',
-    'vendored',
-    '-p',
-    'deltachat_ffi'
-  ], {
-    cwd: coreDir,
-    stdio: 'inherit'
-  }
-)
+spawn('cargo', [ 'update' ], opts)
+spawn('cargo', [
+  'build',
+  '--release',
+  '--features',
+  'vendored',
+  '-p',
+  'deltachat_ffi'
+], opts)
