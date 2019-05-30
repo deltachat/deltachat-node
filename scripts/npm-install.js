88d89a0f355ab9945db42ae377da0f707aa8a8a7
Lars-Magnus Skog <ralphtheninja@riseup.net>
2019-05-30 16:43:25 +0200

    log env

--- scripts/npm-install.js
+++ scripts/npm-install.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const { spawn } = require('./_commons.js')
+console.log('process.env', process.env)
+console.log('process.env.PATH', process.env.PATH)
+console.log('process.env.Path', process.env.Path)
 const opts = {
   cwd: path.resolve(__dirname, '../'),
   env: process.env,
