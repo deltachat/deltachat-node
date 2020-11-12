bee052be54eae820c6f2b90791cc73394f0cb929
Simon Laux <mobile.info@simonlaux.de>
2020-11-12 23:23:20 +0100

    complete converting tests

--- test/test.js
+++ test/test.js
@@ -487,39 +487,40 @@ describe('Offline Tests with unconfigured account', function () {
     expect(dc.getBlockedContacts()).to.be.empty
   })
 
-  it('ChatList methods', function (){
+  it('ChatList methods', function () {
     const ids = [
       dc.createGroupChat('groupchat1'),
       dc.createGroupChat('groupchat11'),
-      dc.createGroupChat('groupchat111')
+      dc.createGroupChat('groupchat111'),
     ]
-  
+
     let chatList = dc.getChatList(0, 'groupchat1', null)
     strictEqual(chatList.getCount(), 3, 'should contain above chats')
     expect(ids.indexOf(chatList.getChatId(0))).not.to.equal(-1)
     expect(ids.indexOf(chatList.getChatId(1))).not.to.equal(-1)
     expect(ids.indexOf(chatList.getChatId(2))).not.to.equal(-1)
-  
+
     const lot = chatList.getSummary(0)
     strictEqual(lot.getId(), 0, 'lot has no id')
     strictEqual(lot.getState(), C.DC_STATE_OUT_DRAFT, 'correct state')
     strictEqual(lot.getText1(), 'Draft', 'text1 is set')
     strictEqual(lot.getText1Meaning(), C.DC_TEXT1_DRAFT, 'text1 meaning')
     expect(
-      lot.getText2().startsWith('Hello, I\'ve just created'),
+      lot.getText2().startsWith("Hello, I've just created"),
       'new group draft message'
     ).to.be.true
     expect(lot.getTimestamp() > 0, 'timestamp set').to.be.true
-  
+
     const text = 'Custom new group message, yo!'
     dc.setStockTranslation(C.DC_STR_NEWGROUPDRAFT, text)
     dc.createGroupChat('groupchat1111')
     chatList = dc.getChatList(0, 'groupchat1111', null)
     strictEqual(
-      chatList.getSummary(0).getText2(), text,
+      chatList.getSummary(0).getText2(),
+      text,
       'custom new group message'
     )
-  
+
     dc.setChatVisibility(ids[0], C.DC_CHAT_VISIBILITY_ARCHIVED)
     chatList = dc.getChatList(C.DC_GCL_ARCHIVED_ONLY, 'groupchat1', null)
     strictEqual(chatList.getCount(), 1, 'only one archived')
@@ -527,23 +528,226 @@ describe('Offline Tests with unconfigured account', function () {
 })
 
 describe('Integration tests', function () {
+  this.timeout(60 * 1000) // increase timeout to 1min
   /** @type {DeltaChat} */
   let dc = null
+  let dc2 = null
+  let directory = ''
+  let account = null
 
   this.beforeEach(async function () {
     dc = new DeltaChat()
-    await dc.open(tempy.directory())
+    directory = tempy.directory()
+    await dc.open(directory)
   })
 
   this.afterEach(async function () {
-    await dc.stopIO()
-    dc.close()
-    dc = null
+    if (dc) {
+      try {
+        try {
+          dc.stopIO()
+          dc2.stopIO()
+        } catch (_e) {}
+        dc.close()
+        dc = null
+        if (dc2) {
+          try {
+            dc2.close()
+          } catch (_e) {}
+          dc2 = null
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    directory = ''
   })
-  // it('opens a context', async function () {
-  //   const dc = new DeltaChat()
-  //   await dc.open(tempy.directory())
-  //   strictEqual(dc.isConfigured(), false)
-  //   dc.close()
-  // })
+
+  this.beforeAll(async function () {
+    if (!process.env.DCC_NEW_TMP_EMAIL) {
+      console.log(
+        'Missing DCC_NEW_TMP_EMAIL environment variable!, skip intergration tests'
+      )
+      this.skip()
+    }
+
+    account = await DeltaChat.createTempUser(process.env.DCC_NEW_TMP_EMAIL)
+  })
+
+  // TODO 1. to 4. below would cover dc.open() completely
+  // 1. test dc.open() where mkdirp fails (e.g. with no permissions)
+  // 2. test failing dc._open() (what would make it fail in core?)
+  // 3. test setting up context with e2ee_enabled set to false + close
+  // 4. test opening an already configured account (re-open above)
+
+  it('test dc.open() where mkdirp fails (e.g. with no permissions)')
+
+  it('test failing dc._open() (what would make it fail in core?)')
+
+  it('test setting up context with e2ee_enabled set to false + close')
+
+  it('test opening an already configured account (re-open above)')
+
+  it('configure', async function () {
+    strictEqual(dc.isConfigured(), false, 'should not be configured')
+
+    // Not sure what's the best way to check the events
+    // TODO: check the events
+
+    // dc.once('DC_EVENT_CONFIGURE_PROGRESS', (data) => {
+    //   t.pass('DC_EVENT_CONFIGURE_PROGRESS called at least once')
+    // })
+    // dc.on('DC_EVENT_ERROR', (error) => {
+    //   console.error('DC_EVENT_ERROR', error)
+    // })
+    // dc.on('DC_EVENT_ERROR_NETWORK', (first, error) => {
+    //   console.error('DC_EVENT_ERROR_NETWORK', error)
+    // })
+
+    // dc.on('ALL', (event, data1, data2) => console.log('ALL', event, data1, data2))
+
+    await expect(
+      dc.configure({
+        addr: account.email,
+        mail_pw: account.password,
+
+        displayname: 'Delta One',
+        selfstatus: 'From Delta One with <3',
+        selfavatar: join(__dirname, 'fixtures', 'avatar.png'),
+      })
+    ).to.be.eventually.fulfilled
+
+    strictEqual(dc.getConfig('addr'), account.email, 'addr correct')
+    strictEqual(dc.getConfig('displayname'), 'Delta One', 'displayName correct')
+    strictEqual(
+      dc.getConfig('selfstatus'),
+      'From Delta One with <3',
+      'selfStatus correct'
+    )
+    strictEqual(
+      dc.getConfig('selfavatar'),
+      join(directory, 'db.sqlite-blobs', 'avatar.png'),
+      'selfavatar correct'
+    )
+    strictEqual(dc.getConfig('e2ee_enabled'), '1', 'e2ee_enabled correct')
+    strictEqual(dc.getConfig('inbox_watch'), '1', 'inbox_watch')
+    strictEqual(dc.getConfig('sentbox_watch'), '0', 'sentbox_watch')
+    strictEqual(dc.getConfig('mvbox_watch'), '0', 'mvbox_watch')
+    strictEqual(dc.getConfig('mvbox_move'), '0', 'mvbox_move')
+    strictEqual(
+      dc.getConfig('save_mime_headers'),
+      '',
+      'save_mime_headers correct'
+    )
+    strictEqual(
+      dc.getBlobdir(),
+      join(directory, 'db.sqlite-blobs'),
+      'correct blobdir'
+    )
+    strictEqual(dc.isConfigured(), true, 'is configured')
+
+    // whole re-configure to only change displayname: what the heck? (copied this from the old test)
+    await expect(
+      dc.configure({
+        addr: account.email,
+        mail_pw: account.password,
+        displayname: 'Delta Two',
+        selfstatus: 'From Delta One with <3',
+        selfavatar: join(__dirname, 'fixtures', 'avatar.png'),
+      })
+    ).to.be.eventually.fulfilled
+    strictEqual(
+      dc.getConfig('displayname'),
+      'Delta Two',
+      'updated displayName correct'
+    )
+  })
+
+  // TODO send text message to chat, check message count and delivered status etc
+  it('send text message to chat, check message count and delivered status etc')
+
+  // TODO test dc.createChatByMsgId()
+  it('test deaddrop: dc.createChatByMsgId()')
+
+  it('Autocrypt setup - key transfer', async function () {
+    // Spawn a second dc instance with same account
+    // dc.on('ALL', (event, data1, data2) =>
+    //   console.log('FIRST ', event, data1, data2)
+    // )
+    await expect(
+      dc.configure({
+        addr: account.email,
+        mail_pw: account.password,
+
+        displayname: 'Delta One',
+        selfstatus: 'From Delta One with <3',
+        selfavatar: join(__dirname, 'fixtures', 'avatar.png'),
+      })
+    ).to.be.eventually.fulfilled
+    dc.startIO()
+    dc2 = new DeltaChat()
+    await dc2.open(tempy.directory())
+    let setupCode = null
+    const waitForSetupCode = waitForSomething()
+    const waitForEnd = waitForSomething()
+    dc.on('DC_EVENT_MSGS_CHANGED', async (chatId, msgId) => {
+      if (
+        !dc.getChat(chatId).isSelfTalk() ||
+        !dc.getMessage(msgId).isSetupmessage()
+      ) {
+        return
+      }
+      let setupCode = await waitForSetupCode.promise
+      // console.log('incoming msg', { setupCode })
+      const messages = dc.getChatMessages(chatId, 0, 0)
+      expect(messages.indexOf(msgId) !== -1, 'msgId is in chat messages').to.be
+        .true
+      const result = await dc.continueKeyTransfer(msgId, setupCode)
+      expect(result === true, 'continueKeyTransfer was successful').to.be.true
+
+      waitForEnd.done()
+    })
+
+    // dc2.on('ALL', (event, data1, data2) =>
+    //   console.log('SECOND', event, data1, data2)
+    // )
+    await expect(
+      dc2.configure({
+        addr: account.email,
+        mail_pw: account.password,
+
+        displayname: 'Delta One',
+        selfstatus: 'From Delta One with <3',
+        selfavatar: join(__dirname, 'fixtures', 'avatar.png'),
+      })
+    ).to.be.eventually.fulfilled
+    dc2.startIO()
+    setupCode = await dc2.initiateKeyTransfer()
+    waitForSetupCode.done(setupCode)
+    // console.log('setupCode is: ' + setupCode)
+    expect(typeof setupCode).to.equal('string', 'setupCode is string')
+
+    await waitForEnd.promise
+  })
+
+  it('configure using invalid password should fail', async function (){  
+    await expect(dc.configure({
+      addr: 'hpk5@testrun.org',
+      mail_pw: 'asd'
+    })).to.be.eventually.rejected
+  })
 })
+
+/**
+ * @returns {{done: (result?)=>void, promise:Promise<any> }}
+ */
+function waitForSomething() {
+  let resolvePromise
+  const promise = new Promise((res, rej) => {
+    resolvePromise = res
+  })
+  return {
+    done: resolvePromise,
+    promise,
+  }
+}
