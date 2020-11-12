06a164dde2e7fb6f4e9c4fe01d1e26224623b396
Simon Laux <mobile.info@simonlaux.de>
2020-11-12 21:18:28 +0100

    transfer and fix the first tests

--- test/open.js
+++ test/open.js
@@ -3,25 +3,6 @@ const binding = require('../binding')
 const test = require('tape')
 const tempy = require('tempy')
 
-test('open', t => {
-  const dc = new DeltaChat(tempy.directory())
-  dc.open(tempy.directory())
-  t.is(dc.isConfigured(), false)
-  dc.close()
-  t.end()
-})
-test('> missing addr or mail_pw throws', t => {
-  const dc = new DeltaChat()
-  dc.open(tempy.directory())
-  t.throws(function () {
-    dc.configure({ addr: 'delta1@delta.localhost' })
-  }, 'Missing .mailPw', 'missing mailPw throws')
-  t.throws(function () {
-    dc.configure({ mailPw: 'delta1' })
-  }, /Missing \.addr/, 'missing addr throws')
-  dc.close()
-  t.end()
-})
 
 // TODO move to integration tests
 test('> autoconfigure (using invalid password)', t => {
