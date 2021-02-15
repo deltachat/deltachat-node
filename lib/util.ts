aa43d33589dbb77cd8db83640cf5bdbd47629ce9
Simon Laux <mobile.info@simonlaux.de>
2021-02-15 17:32:01 +0100

    Fix `integerToHexColor(colorInt)` function to work correctly with new color generation.

--- lib/util.ts
+++ lib/util.ts
@@ -1,3 +1,6 @@
-export function integerToHexColor(integerColor: number) {
-  return '#' + integerColor.toString(16)
+/**
+ * @param integerColor expects a 24bit rgb integer (left to right: 8bits red, 8bits green, 8bits blue)
+ */
+export function integerToHexColor(integerColor: number) { 
+  return '#' + (integerColor + 16777216).toString(16).substring(1)
 }
