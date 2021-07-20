b26b036ed255e8d82f4d57892d09317d1a71fc12
jikstra <jikstra@disroot.org>
2021-07-20 21:15:00 +0200

    Try fixing windows...

--- scripts/install.js
+++ scripts/install.js
@@ -1,8 +1,8 @@
-const {exec} = require('child_process');
+const {execSync} = require('child_process');
 
 if (process.env.USE_SYSTEM_LIBDELTACHAT === 'true') {
   console.log('[i] USE_SYSTEM_LIBDELTACHAT is true, using pkg-config to retrieve lib paths and cflags')
-  exec('npm run install:system')	
+  execSync('npm run install:system')	
 } else {
-  exec('npm run install:vendored')	
+  execSync('npm run install:vendored')	
 }
