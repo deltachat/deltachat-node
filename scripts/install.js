b7b3f12f2a3b64e28bd2110400e294fb4c10f1f1
jikstra <jikstra@disroot.org>
2021-07-22 19:08:21 +0200

    As we bundle the prebuilds, we don't need node-gyp-build anymore. Also typescript is not needed if npm i retrieves deltachat-node from npm (the typescript transpilation is part of the package)

--- scripts/install.js
+++ scripts/install.js
@@ -1,8 +1,22 @@
 const {execSync} = require('child_process');
+const isProduction = process.env.NODE_ENV && process.env.NODE_ENV === 'production'
 
+const run = (cmd) => {
+  console.log('[i] running `' + cmd '`'
+  execSync(cmd)
+}
+
+// Fetch submodule if needed
+if(!isProduction) {
+  console.log('Retrieving deltachat-core-rust submodule')
+  run('npm run submodule')
+}
+
+// Build bindings
 if (process.env.USE_SYSTEM_LIBDELTACHAT === 'true') {
-  console.log('[i] USE_SYSTEM_LIBDELTACHAT is true, using pkg-config to retrieve lib paths and cflags')
-  execSync('npm run install:system')	
-} else {
-  execSync('npm run install:vendored')	
+  console.log('[i] USE_SYSTEM_LIBDELTACHAT is true, rebuilding c bindings and using pkg-config to retrieve lib paths and cflags of libdeltachat')
+  run('npm run build:bindings:c:c)	
+} else if(!isProduction) {
+  console.log('[i] Building rust core & bindings')
+  run('npm run build')
 }
